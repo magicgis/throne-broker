060279264431fd40ed749069a9b7dab486d6b92b
kevto <berendsen.kevin@gmail.com>
2016-07-24 14:43:03 +0200

    Adding config and properties files. Using assembly to package the project with all the dependencies. Still need to work on the shell script to execute the application because it can not find my own Server class yet.

---
+++ src/main/java/nl/kevinberendsen/throne/Server.java
@@ -0,0 +1,11 @@
+package nl.kevinberendsen.throne;
+
+/**
+ * Created by nedermail on 24/07/16.
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from server..");
+    }
+}
