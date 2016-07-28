9d5b6b21324f2cd9079c4e35fc8de5df87169bb2
kevto <info@kevinberendsen.nl>
2016-07-29 01:11:16 +0200

    Overrides every method of AbstractInterceptHandler in ThroneInterceptHandler. Logging everything in debug mode.

--- src/main/java/nl/kevinberendsen/throne/ThroneInterceptHandler.java
+++ src/main/java/nl/kevinberendsen/throne/ThroneInterceptHandler.java
@@ -1,12 +1,41 @@
 package nl.kevinberendsen.throne;
 
 import io.moquette.interception.AbstractInterceptHandler;
-import io.moquette.interception.messages.InterceptPublishMessage;
+import io.moquette.interception.messages.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by nedermail on 29/07/16.
  */
 public class ThroneInterceptHandler extends AbstractInterceptHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ThroneInterceptHandler.class);
+
+    @Override
+    public void onConnect(InterceptConnectMessage msg) {
+        LOG.debug(String.format("onConnect: %s is trying to connect.", msg.getUsername()));
+    }
+
+    @Override
+    public void onDisconnect(InterceptDisconnectMessage msg) {
+        LOG.debug(String.format("onDisconnect: %s is trying to disconnect.", msg.getClientID()));
+    }
+
+    @Override
+    public void onSubscribe(InterceptSubscribeMessage msg) {
+        LOG.debug(String.format("onSubscribe: %s is trying to subscribe on %s with QOS %s.",
+                msg.getClientID(),
+                msg.getTopicFilter(),
+                msg.getRequestedQos()));
+    }
+
+    @Override
+    public void onUnsubscribe(InterceptUnsubscribeMessage msg) {
+        LOG.debug(String.format("onUnsubscribe: %s is trying to unsubscribe on %s.",
+                msg.getClientID(),
+                msg.getTopicFilter()));
+    }
 
     @Override
     public void onPublish(InterceptPublishMessage msg) {
